3cd7db63f51cbddb7c14d375ee1b5771a5775f7d
Madhur Toppo <madhur.toppo@gmail.com>
2025-06-26 00:37:15 +0200

    SD-25 Migrate from Feign Client to REST Client

--- orderservice/src/main/java/com/madhurtoppo/microservices/orderservice/client/InventoryClient.java
+++ orderservice/src/main/java/com/madhurtoppo/microservices/orderservice/client/InventoryClient.java
@@ -1,13 +1,10 @@
 package com.madhurtoppo.microservices.orderservice.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "inventoryservice", url = "http://localhost:8083")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventories/stock/{skuCode}/{quantity}")
+    @GetExchange("/api/inventories/stock/{skuCode}/{quantity}")
     boolean isInStock(@PathVariable("skuCode") String skuCode, @PathVariable("quantity") Integer quantity);
 }
